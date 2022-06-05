e52040f2f5529eba31726d585cea2a856eb36acb
omarghanem1210 <omarghanem1210@gmail.com>
2022-06-05 15:50:22 +0200

    Added The tests for the powerMod function and added an empy Main class

--- src/PiGenerator.java
+++ src/PiGenerator.java
@@ -1,2 +1,5 @@
 public class PiGenerator {
+    public static int powerMod(int a, int b, int m){
+        return -1;
+    }
 }
--- src/PiGeneratorTest.java
+++ src/PiGeneratorTest.java
@@ -1,2 +1,30 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class PiGeneratorTest {
+    @Test
+    public void basicTest(){
+        assertEquals(8, PiGenerator.powerMod(8, 3, 9));
+    }
+
+    @Test
+    public void zeroTest(){
+        assertEquals(0, PiGenerator.powerMod(0, 6, 5));
+    }
+
+    @Test
+    public void unityTest(){
+        assertEquals(1, PiGenerator.powerMod(3, 1, 2));
+    }
+
+    @Test
+    public void negativeTest(){
+        assertEquals(6, PiGenerator.powerMod(-2, 6, 7));
+    }
+
+    @Test
+    public void negativeTest1(){
+        assertEquals(-4, PiGenerator.powerMod(-2, 6, -5));
+    }
 }
