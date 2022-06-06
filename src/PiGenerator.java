993f5efbbad880cfb549c2dc085542377ee87648
omarghanem1210 <omarghanem1210@gmail.com>
2022-06-06 19:27:10 +0200

    Something

--- src/PiGenerator.java
+++ src/PiGenerator.java
@@ -19,4 +19,61 @@ public class PiGenerator {
             return a * exp_by_squaring(a * a, (b - 1) / 2);
         }
     }
+    public static int[] computePiInHex(int precision){
+        int[] piInHex = new int[precision];
+        for (int i = 0; i < precision; i++) {
+            piInHex[i] = piDigit(i+1); // +1 to remove 3 from pi.
+        }
+
+        return piInHex;
+
+    }
+
+    /**
+     * Computes the nth digit of Pi in base-16.
+     *
+     * If n < 0, return -1.
+     *
+     * @param n The digit of Pi to retrieve in base-16.
+     * @return The nth digit of Pi in base-16.
+     */
+    public static int piDigit(int n) {
+        if (n < 0) return -1;
+
+        n -= 1;
+        double x = 4 * piTerm(1, n) - 2 * piTerm(4, n) -
+                piTerm(5, n) - piTerm(6, n);
+        x = x - Math.floor(x);
+
+        return (int)(x * 16);
+    }
+
+    private static double piTerm(int j, int n) {
+        // Calculate the left sum
+        double s = 0;
+        for (int k = 0; k <= n; ++k) {
+            int r = 8 * k + j;
+            s += powerMod(16, n-k, r) / (double) r;
+            s = s - Math.floor(s);
+        }
+
+        // Calculate the right sum
+        double t = 0;
+        int k = n+1;
+        // Keep iterating until t converges (stops changing)
+        while (true) {
+            int r = 8 * k + j;
+            double newt = t + Math.pow(16, n-k) / r;
+            if (t == newt) {
+                break;
+            } else {
+                t = newt;
+            }
+            ++k;
+        }
+
+        return s+t;
+    }
 }
+
+
--- src/PiGeneratorTest.java
+++ src/PiGeneratorTest.java
@@ -3,6 +3,9 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PiGeneratorTest {
+    private static final String PI_FIRST_HUNDRED = "243f6a8885a308d313198a2e03707344a4093822299f31d0082efa98ec4e"
+            + "6c89452821e638d01377be5466cf34e90c6cc0ac";
+
     @Test
     public void basicTest(){
         assertEquals(8, PiGenerator.powerMod(8, 3, 9));
@@ -31,5 +34,27 @@ public class PiGeneratorTest {
     @Test
     public void largeNumberTest(){
         assertEquals(448, PiGenerator.powerMod(4, 17, 576));
+    }
+
+    @Test
+    public void onePrecisionTest(){
+        assertEquals(PI_FIRST_HUNDRED.substring(0, 1), Integer.toHexString(PiGenerator.computePiInHex(1)[0]));
+    }
+
+    @Test
+    public void tenPrecisionTest(){
+        int[] list = PiGenerator.computePiInHex(10);
+        for (int i = 0; i < 10; i++){
+            assertEquals(PI_FIRST_HUNDRED.substring(i, i+1), Integer.toHexString(list[i]));
+        }
+    }
+
+    @Test
+    public void hundredPrecisionTest(){
+        for (int i = 0; i < 100; i++){
+            int[] list = PiGenerator.computePiInHex(100);
+            assertEquals(PI_FIRST_HUNDRED.substring(i, i+1), Integer.toHexString(list[i]));
+        }
+
     }
 }
